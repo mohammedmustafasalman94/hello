7c684512308dc71ec12addae77aa73bc170224ba
mustafasalmanmohammed <mustafasalmanmohammed@gmail.com>
2023-09-19 17:02:25 +0530

    hello lara

---
+++ F.java
@@ -0,0 +1,14 @@
+class F 
+{
+	public static void main(String[] args) 
+	{
+		double var=1.0;
+		System.out.println(var);
+		var=2.03;
+		System.out.println(var);
+		var=3.40;
+		System.out.println(var);
+		var=0.0;
+		System.out.println(var);
+	}
+}
